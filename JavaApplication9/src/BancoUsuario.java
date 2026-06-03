cbd1fd73d2795a5615d4781edd93b30c30ebfbdf
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2026-06-02 21:50:16 -0300

    Add files via upload

---
+++ JavaApplication9/src/BancoUsuario.java
@@ -0,0 +1,37 @@
+
+ import java.util.ArrayList;
+ 
+ 
+/**
+ *
+ * @author VINICIUSDASILVACARDO
+ */
+public class BancoUsuario {
+    private static ArrayList<Usuario> usuarios = new ArrayList<>();
+    public static void adicionarUsuario(Usuario usuario){
+        usuarios.add(usuario);
+ }
+
+  public static boolean ValidarLogin (String email, String senha){
+        for (Usuario u : usuarios){
+            if (u.getEmail().equals(email)&& u.getSenha().equals(senha)){
+                return true;
+            }
+        }
+     return false;
+    
+}
+
+public static  String ValidarNome(String email, String senha){
+for (Usuario u : usuarios){
+if (u.getEmail().equals(email) && u.getSenha().equals(senha)){
+return u.getNome();
+}
+}
+return "Sem Nome";
+
+
+}
+}
+
+
